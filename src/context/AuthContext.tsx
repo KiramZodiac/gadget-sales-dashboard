4db75f3ba9b3086993448fbcf009807f0a3bf51e
KiramZodiac <kiramzodiac@gmail.com>
2025-05-28 16:28:05 +0300

    added google authentication

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -88,7 +88,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/dashboard`,
+          redirectTo: `${window.location.origin}/`,
           scopes: 'email profile',
         },
       });
